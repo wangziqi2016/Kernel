1e737f99dcd8cb63a84dcf2fa12e63d176b25d25
Ziqi Wang <ziqiw@mit.edu>
2017-10-17 02:34:16 -0400

    Change boot sector to have print

--- src/bootsect/bootsect.asm
+++ src/bootsect/bootsect.asm
@@ -5,35 +5,75 @@
 section .text 
 	org	7C00H
 	jmp	start
+
 start:
   ; boot section goes after
 	; AH = 00 - set mode; AL = 03h - 80*25@16
 	mov ax, 0003h
 	int 10h
 
+  ; Set DS=SS; ES=0B800h
   xor ax, ax
   mov ds, ax
+	; Set up stack end: 0x0FFF0
+	; Note that this sector is on address 0x07C00 - 0x07DFF
+	mov ss, ax
+	mov sp, 0FFF0h
+
   mov ax, 0b800h
   mov es, ax
 
-  xor di, di
-  mov si, str
-	
-loop_body:
+  mov si, str1
+	call near print_msg
+
+start_load:
+  jmp start_load
+  ; leave space to read disk parameters
+  sub sp, 001Eh
+
+  mov si, sp
+  xor dx, dx
+  mov ah, 48h
+
+  int 13h
+  jc print_read_param_error
+
+this_line:
+  jmp this_line
+
+print_msg:
+  push di
+  mov di, [video_offset]
+print_msg_body:
   mov al, [ds:si]
   test al, al
-  je after_loop
+  je print_msg_ret
   mov [es:di], al
-	inc si
-	inc di
-	inc di
-  jmp loop_body
+  inc si
+  inc di
+  inc di
+  jmp print_msg_body
+print_msg_ret:
+	mov [video_offset], di
+	pop di
+	retn
 
-after_loop:
-	jmp after_loop
+print_read_param_error:
+  mov si, str2
+	;call 
 
-str:
-  db "Hello World!", 0
-  ; This line pads the first sector to 512 bytes
-	times 510-($-$$) DB 0 
+str1:
+  db "Loading boot sectors... ", 0
+str2:
+  db "Error reading disk parameters", 0
+str3:
+  db "Error reading sectors", 0
+
+boot_drive:
+  db 0
+video_offset:
+  dw 0
+
+; This line pads the first sector to 512 bytes
+  times 510-($-$$) DB 0 
   dw 0AA55H
