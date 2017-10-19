1697c50e9160a60599b99d9a3abed8d621b2bb6f
Ziqi Wang <ziqiw@mit.edu>
2017-10-19 00:00:32 -0400

    Adding keyboard

---
+++ src/bootsect/loader_kbd.asm
@@ -0,0 +1,18 @@
+
+;
+; loader_kbd.asm - This file implements the keyboard driver
+;
+
+
+
+; Only the first 127 entries are useful
+.kbd_scan_code_map: 
+;  1    2    3    4    5    6    7    8    9    A    B    C    D    E
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 1 
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 2
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 3
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 4
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 5
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 6
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 7
+db 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h, 00h   ; 8
