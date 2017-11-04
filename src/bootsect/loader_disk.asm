ba2d8fe0b05df870d343d14378a6ebcaa318ba28
Ziqi Wang <ziqiw@mit.edu>
2017-11-04 18:57:33 -0400

    Adding loader disk module

---
+++ src/bootsect/loader_disk.asm
@@ -0,0 +1,8 @@
+_loader_disk_start:
+;
+; loader_disk.asm - This file contains disk driver and I/O routine
+;
+
+  ; This function detects all floppy and hard disks using BIOS routine
+disk_init:
+  
