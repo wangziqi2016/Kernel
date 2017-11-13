8cbf660e22db3850a2412ec03498bc9a4781c4e6
Ziqi Wang <ziqiw@mit.edu>
2017-11-13 06:24:05 -0500

    Adding mem_read(), write and free

--- src/util/ofs.c
+++ src/util/ofs.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdarg.h>
 
 #define DEFAULT_SECTOR_SIZE 512
 
@@ -18,19 +19,68 @@ enum STORAGE_TYPE {
 };
 
 // This defines the storage we use
-typedef struct Storage_p {
+typedef struct Storage_t {
   int type;
   // Number of bytes per sector
-  int sector_size;
+  size_t sector_size;
+  // Number of sectors in total in the storage
+  size_t sector_count;
   // Either we use it as a file pointer or as a data pointer
   union {
     FILE *fp;
     uint8_t *data_p;
   };
   // Read and write function call backs
-  void (*read)(struct Storage_p *disk_p, uint64_t lba, void *buffer);
-  void (*write)(struct Storage_p *disk_p, uint64_t lba, void *buffer);
+  void (*read)(struct Storage_t *disk_p, uint64_t lba, void *buffer);
+  void (*write)(struct Storage_t *disk_p, uint64_t lba, void *buffer);
+  void (*free)(struct Storage_t *disk_p);
 } Storage;
+
+/*
+ * fatal_error() - Reports error and then exit
+ */
+void fatal_error(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  exit(1);
+}
+
+/*
+ * mem_read() - Reads a sector into the given buffer
+ */
+void mem_read(Storage *disk_p, uint64_t lba, void *buffer) {
+  if(lba >= disk_p->sector_count) {
+    fatal_error("Invalid LBA for read: %lu", lba);
+  }
+
+  size_t offset = lba * disk_p->sector_size;
+  memcpy(buffer, disk_p->data_p + offset, disk_p->sector_size);
+
+  return;
+}
+
+/*
+ * mem_write() - Writes a buffer of data into the memory
+ */
+void mem_write(Storage *disk_p, uint64_t lba, void *buffer) {
+  if(lba >= disk_p->sector_count) {
+    fatal_error("Invalid LBA for read: %lu", lba);
+  }
+
+  size_t offset = lba * disk_p->sector_size;
+  memcpy(disk_p->data_p + offset, buffer, disk_p->sector_size);
+
+  return;
+}
+
+/*
+ * mem_free() - Frees a memory object
+ */
+void mem_free(Storage *disk_p) {
+  free(disk_p->data_p);
+  return;
+}
 
 int main() {
   return 0;
