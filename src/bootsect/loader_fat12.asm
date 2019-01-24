4308f1617ccd98bb47220696ffa310025e2b2bde
Ziqi Wang <ziqiw@mit.edu>
2019-01-24 15:58:11 -0500

    Working on checking FAT12

--- src/bootsect/loader_fat12.asm
+++ src/bootsect/loader_fat12.asm
@@ -13,6 +13,7 @@ struc fat12_param           ; This defines FAT12 file system metadata
   .num_fat:          resb 1 ; Number of FAT tables (Sector 1, 0x10)
   .fat_size:         resw 1 ; Number of sectors per FAT (Sector 1, 0x16)
   .data_begin:       resw 1 ; Offset of sector that data begins (from 0), derived from above values
+  .padding:          resb 1 ; Padding
   .size:
 endstruc
 
@@ -22,17 +23,17 @@ fat12_init:
   push bp
   mov bp, sp
   push bx                                   ; [BP - 2] - Reg saving
-  push si
+  push si                                   ; [BP - 4] - Reg saving
   mov bx, [disk_mapping]
-  mov cx, [disk_mapping_size]
-.check_fat12:
-.addr_hi           equ -4                   ; Local variables
-.addr_lo           equ -6
-.letter            equ -8
-  push word 0                               ; [BP - 4] - Addr hi
-  push word 26h                             ; [BP - 6] - Addr lo; Byte offset 0x26 (extended boot record)
-  push word [bx + disk_mapping.letter]      ; [BP - 8] - Current letter of the disk
-  mov ax, DISK_OP_READ                      ; Perform read
+  mov si, [disk_mapping_size]
+.body:
+.addr_hi           equ -6                   ; Local variables
+.addr_lo           equ -8
+.letter            equ -10
+  push word 0                               ; [BP - 6] - Addr hi
+  push word 26h                             ; [BP - 8] - Addr lo; Byte offset 0x26 (extended boot record)
+  push word [bx + disk_mapping.letter]      ; [BP - 10] - Current letter of the disk
+  mov ax, si                                ; Perform read
   call disk_op_word                         ; Read the first sector of the current disk
   jc .err                                   ; Do not clear stack because we read multiple words
   cmp ax, 28h
@@ -41,7 +42,9 @@ fat12_init:
   je .found
 .continue:
   add bx, disk_param.size
-  loop .check_fat12
+  dec si
+  test si, si
+  jnz .body
 .return:
   pop si
   pop bx
@@ -53,7 +56,23 @@ fat12_init:
   call mem_get_sys_bss                      ; Allocate a perameter entry for FAT 12
   jc .err                                   ; Usually means sys static mem runs out
   mov [bx + disk_param.fsptr], ax           ; Save it in the fsptr field of disk param
-  mov ax, DISK_OP_READ
+  mov bx, ax                                ; BX = Ptr to the FAT12 param
+  xor ax, ax
+  mov [bp + .addr_lo], ax                   ; Clear high bits of the address (we only use low 256 bytes for sure)
+.offset_table:                              ; Defines the metadata we copy from sector 0
+.db 0dh, fat12_param.cluster_size
+.db 0eh, fat12_param.reserved
+.db 10h, fat12_param.num_fat
+.db 16h, fat12_param.fat_size
+  mov byte [bp + .addr_lo], 0dh             ; Update address we read
+  mov ax, si                                ; Read 0x0D byte
+  call disk_op_word
+  jc .err
+  mov [bx + fat12_param.cluster_size], al
+  mov byte [bp + .addr_lo], 0eh             
+  mov ax, si                                ; Read 0x0E word
+  call disk_op_word
+  jc .err
 .err:
   push ds
   push fat12_init_err
